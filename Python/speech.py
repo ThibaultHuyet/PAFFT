f5c3e9c0ba52ec0484384ae1dcbbb1991b04ae2a
Thibault Huyet <thibault.huyet@gmail.com>
2017-12-05 04:28:54 +0000

    added more on the graphing

--- Python/speech.py
+++ Python/speech.py
@@ -1,7 +1,10 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 import paho.mqtt.client as mqtt
 
+
+samplefreq = 44100
 
 def on_connect(client, userdata, flags, rc):
     """
@@ -16,10 +19,17 @@ def on_message(client, userdata, msg):
     """
     j = json.loads(msg.payload)
 
+    binnum = len(j['mag'])
     maxj = max(j['mag'])
-    normj = [x / maxj for x in j['mag']]
 
-    plt.plot(normj)
+    reso = samplefreq / binnum
+    
+    x_temp = np.arange(0, binnum / 2)
+    x = [x * reso for x in x_temp]
+
+    normj = [x / maxj for x in j['mag']]
+    plt.plot(x, normj[:2048])
+    plt.xscale('log')
     plt.show()
 
 client = mqtt.Client()
