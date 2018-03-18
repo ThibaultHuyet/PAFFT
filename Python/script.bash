7360f975395d0a33a0331147837a6019bb559351
Ubuntu <ubuntu@ip-172-31-37-32.eu-west-1.compute.internal>
2018-03-18 17:36:07 +0000

    Fixed bash script

--- Python/script.bash
+++ Python/script.bash
@@ -2,9 +2,8 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 -y
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
-sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y --allow-unauthenticated
 
 sudo apt-get update
-sudo apt-get install -y python3.6 mongodb-org python3-pip mosquitto mosquitto-clients
-
+sudo apt-get install -y python3.6 mongodb-org python3-pip mosquitto mosquitto-clients --allow-unauthenticated
 sudo service mongod start
