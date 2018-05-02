fa783ff8a0c55cf3d0223b24cdf91296d2dce3c5
Thibault Huyet <thibault.huyet@gmail.com>
2018-05-02 16:40:05 +0100

    Gave up on JSON idea for now, just a string

--- shell_scripts/performance.sh
+++ shell_scripts/performance.sh
@@ -1,14 +1,14 @@
 end=$((SECONDS+5))
 
-echo ${stringarray[1]}
-echo ${stringarray[2]}
-echo ${stringarray[3]}
-echo ${stringarray[10]}
-
 while [ $SECONDS -lt $end ];
 do
 	var=$(ps -aux | grep "./number")
 	stringarray=($var)
 
-	mosquitto_pub -t 'hello' -m "{'PID' : ${stringarray[1]}, 'CPU%' : ${stringarray[2]}, 'MEM%' : ${stringarray[3]}, 'Process Name' : ${stringarray[10]}}"
+	pid=${stringarray[1]}
+	cpu=${stringarray[2]}
+	mem=${stringarray[3]}
+	pname=${stringarray[10]}
+	
+	mosquitto_pub -t 'hello' -m "$pid $cpu $mem $pname"
 done
