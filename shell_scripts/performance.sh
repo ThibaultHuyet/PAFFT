a37c8cd2ec417213bd78bb1a5e0444843bd855ac
Thibault Huyet <thibault.huyet@gmail.com>
2018-05-02 16:11:47 +0100

    Improved performance script

--- shell_scripts/performance.sh
+++ shell_scripts/performance.sh
@@ -10,8 +10,5 @@ do
 	var=$(ps -aux | grep "./number")
 	stringarray=($var)
 
-	echo ${stringarray[1]}
-	echo ${stringarray[2]}
-	echo ${stringarray[3]}
-	echo ${stringarray[10]}
+	mosquitto_pub -t 'hello' -m "{'PID' : ${stringarray[1]}, 'CPU%' : ${stringarray[2]}, 'MEM%' : ${stringarray[3]}, 'Process Name' : ${stringarray[10]}}"
 done
