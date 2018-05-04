618caed04e0f6ebb4a5efd0f65acc0b32079387f
Thibault Huyet <thibault.huyet@gmail.com>
2018-05-04 15:40:14 +0100

    Script now sends performance stats that are most important

--- shell_scripts/performance.sh
+++ shell_scripts/performance.sh
@@ -2,13 +2,20 @@ end=$((SECONDS+5))
 
 while [ $SECONDS -lt $end ];
 do
-	var=$(ps -aux | grep "./number")
-	stringarray=($var)
+        total=$(ps --no-headers -a -x -o pid,%cpu,%mem,command | awk '{cpu += $2; mem += $3} END {print cpu, mem}')
+        dockerd=$(ps --no-headers -a -x -o pid,%cpu,%mem,command | grep dockerd)
+        container=$(ps --no-headers -a -x -o pid,%cpu,%mem,command | grep docker-cont)
+        program=$(ps --no-headers -a -x -o pid,%cpu,%mem,command | grep main)
 
-	pid=${stringarray[1]}
-	cpu=${stringarray[2]}
-	mem=${stringarray[3]}
-	pname=${stringarray[10]}
-	
-	mosquitto_pub -t 'hello' -m "$pid $cpu $mem $pname"
+        totalarray=($total)
+        dockerdarray=($dockerd)
+        containerarray=($container)
+        programarray=($program)
+
+        mosquitto_pub -h '172.28.14.95' -t 'hello' -m '{"performance" : {"total" :
+{"cpu" :'"${totalarray[0]}"', "mem" :'"${totalarray[1]}"'}, "dockerd" : {"cpu":
+'"${dockerdarray[1]}"', "mem":'"${dockerdarray[2]}"'}, "container":{"cpu":
+'"${containerarray[1]}"', "mem":'"${containerarray[2]}"'},
+"program":{"cpu":'"${programarray[1]}"', "mem":'"${programarray[2]}"'}}}'
+
 done
